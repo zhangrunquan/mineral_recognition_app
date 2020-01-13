e28341266a3a87e7a0faccc7b48a4fa531017a15
zhangrunquan <34376163+zhangrunquan@users.noreply.github.com>
2020-01-13 21:30:41 +0800

    1.13 更新用于测试的flask代码

--- flask_server/api.py
+++ flask_server/api.py
@@ -18,8 +18,8 @@ def upload_json():
     string=request.form.get('content')
     import json
     decoder=json.JSONDecoder()
-    dic=decoder.decode(string)
-    return jsonify({'type':dic['content']}) # 返回上传的内容
+    dic=decoder.decode(string) # dic是提交的json解析生成的字典
+    return jsonify({'type':'crystal'})
     
 
 @bp.route('/api/upload/image',methods = ['POST'])
@@ -30,12 +30,12 @@ def upload_image():
     
     if file:
         file.save(undecoded_filename)
-        
+
     with open(undecoded_filename,'r') as f:
         string=f.read()
         decoded=base64.b64decode(string)
         with open(decoded_filename,'wb') as f2:
-            f2.write(decoded)
+            f2.write(decoded) # f2是正常的jpg文件
 
-    return jsonify({'type':'test return'}) # 返回上传的内容
+    return jsonify({'type':'crystal'}) 
     
